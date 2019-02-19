2af6fc66228cbb4c294f3da8f95be57e2f89febe
Thomas B Klapheke <tommyklapheke@yahoo.com>
2019-02-18 22:19:56 -0500

    Changed viewWillLayoutSubviews to update scene when needed, sets the scale, and makes sure the scene fills the entire screen. Changed supportedInterfaceOrientations to be sure that we have the device landscape.

--- Pierre The Penguin/Pierre The Penguin/GameViewController.swift
+++ Pierre The Penguin/Pierre The Penguin/GameViewController.swift
@@ -12,23 +12,22 @@ import GameplayKit
 
 class GameViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
         
         if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
+            //Load the SKScene
             if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
+                //Set scale mode to aspect fill
                 scene.scaleMode = .aspectFill
-                
-                // Present the scene
+                // Set size to fit view exactly
+                scene.size = view.bounds.size
+                //Show new scene
                 view.presentScene(scene)
             }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
+        view.ignoresSiblingOrder = true
+        view.showsFPS = true
+        view.showsNodeCount = true
         }
     }
 
@@ -37,11 +36,7 @@ class GameViewController: UIViewController {
     }
 
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
+        return .landscape
     }
 
     override var prefersStatusBarHidden: Bool {
